ed9cf696068091f7e693cd3ba75954fd5e4c5e5d
Gyles Batara and Patrick Napper <gyles@>
2017-10-25 16:25:56 -0700

    Create test for all specs, all pass.

--- Restaurant.Tests/ModelTests/RestaurantTests.cs
+++ Restaurant.Tests/ModelTests/RestaurantTests.cs
@@ -9,11 +9,11 @@ namespace BestRestaurants.Tests
 
   [TestClass]
 
-  public class RestaurantTests : IDisposable
+  public class RestaurantsTest : IDisposable
   {
-    public RestaurantTests()
+    public RestaurantsTest()
     {
-      DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=restaurants_test;";
+      DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=restaurant_test;";
     }
 
     public void Dispose()
@@ -30,6 +30,81 @@ namespace BestRestaurants.Tests
 
       //Assert
       Assert.AreEqual(0, result);
+    }
+
+    [TestMethod]
+    public void Equals_OverrideTrueIfNamesAreSame_Restaurant()
+    {
+      //Arrange, Act
+      Restaurant firstRestaurant = new Restaurant("Pizza Hut", "Italian", 1, 20);
+      Restaurant secondRestaurant = new Restaurant("Pizza Hut", "Italian", 1, 20);
+
+      //Assert
+      Assert.AreEqual(firstRestaurant, secondRestaurant);
+    }
+
+    [TestMethod]
+    public void Save_SavesToDatabase_RestaurantList()
+    {
+      //Arrange
+      Restaurant testRestaurant = new Restaurant("Pizza Hut", "Italian", 1, 20, 1);
+
+      //Act
+      testRestaurant.Save();
+      List<Restaurant> result = Restaurant.GetAll();
+      List<Restaurant> testList = new List<Restaurant>{testRestaurant};
+
+      //Assert
+      CollectionAssert.AreEqual(testList, result);
+    }
+
+    [TestMethod]
+    public void Save_AssignsIdToObject_Id()
+    {
+      //Assert
+      Restaurant testRestaurant = new Restaurant("Pizza Hut", "Italian", 1, 20);
+
+      //Act
+      testRestaurant.Save();
+      Restaurant savedRestaurant = Restaurant.GetAll()[0];
+
+      int result = savedRestaurant.GetId();
+      int testId = testRestaurant.GetId();
+
+      //Assert
+      Assert.AreEqual(testId, result);
+    }
+
+    [TestMethod]
+    public void Find_FindRestaurantInDatabase_Restaurant()
+    {
+      // Arrange
+      Restaurant testRestaurant = new Restaurant("Diner Yukihira", "Japanese", 1, 30);
+      testRestaurant.Save();
+
+      // Act
+      Restaurant foundRestaurant = Restaurant.Find(testRestaurant.GetId());
+
+      // Assert
+      Assert.AreEqual(testRestaurant, foundRestaurant);
+    }
+
+    [TestMethod]
+    public void Update_UpdatesRestaurantInDatabase_String()
+    {
+      // Arrange
+      string name = "Trattoria Aldini";
+      Restaurant testRestaurant = new Restaurant(name, "Italian", 3, 10);
+      testRestaurant.Save();
+      string newName = "Osteria La Spriga";
+
+      // Act
+      testRestaurant.UpdateName(newName);
+
+      string result = Restaurant.Find(testRestaurant.GetId()).GetName();
+
+      // Assert
+      Assert.AreEqual(newName, result);
     }
   }
 }
